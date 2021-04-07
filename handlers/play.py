d589f1becfbeb564e05bbdb77b987acf08cad1ec
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-07 15:46:23 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -27,7 +27,7 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="🔊 Channel",
+                        text="🔊 Moi Sweet Master",
                         url="https://t.me/Infinity_BOTs")
                    
                 ]
@@ -59,7 +59,7 @@ async def play(_, message: Message):
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
-        photo="https://telegra.ph/file/a4fa687ed647cfef52402.jpg",
+        photo="https://telegra.ph/file/80f5a0b6e3f5cdfe34e75.jpg",
         reply_markup=keyboard,
         caption="▶️ **Playing** here the song requested by {}!".format(
         message.from_user.mention()
